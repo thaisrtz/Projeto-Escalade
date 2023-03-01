e84c76c87ddab7bcaf2131c4fb9bd76e44a20835
thaisrtz <thaistzanella@gmail.com>
2023-03-01 17:56:24 -0300

    01.03

--- pythonProject/forca.py
+++ pythonProject/forca.py
@@ -1,28 +1,22 @@
 import random
 
-
 def jogar():
 
     print_opening_message()
     secret_word = secret_word_generator()
     right_letters = secret_right_letters(secret_word)
+    print(right_letters)
 
     defeat = False
     victory = False
     wrong_guess = 0
 
-    print(right_letters)
     while (not defeat and not victory):
 
-        guess = input("Digite uma letra: ")
-        guess = guess.strip().upper()
+        guess = receiving_guess()
 
         if(guess in secret_word):
-            index = 0
-            for letter in secret_word:
-                if (guess == letter):
-                    right_letters[index] = letter
-                index += 1
+            right_guess_marker(guess, right_letters, secret_word)
 
         else:
             wrong_guess += 1
@@ -32,12 +26,10 @@ def jogar():
         victory = "_" not in right_letters
         print(right_letters)
 
-    print("Fim do jogo!")
     if (victory):
-        print("Você ganhou!")
+        print_victory_message()
     if (defeat):
-        print ("Você perdeu!")
-        print("A palavra secreta era: ", secret_word)
+        print_defeat_message()
 
 def print_opening_message():
     print("*****************************")
@@ -59,6 +51,25 @@ def secret_word_generator():
 
 def secret_right_letters(word):
     return ["_" for letter in word]
+
+def receiving_guess():
+    guess = input("Digite uma letra: ")
+    guess = guess.strip().upper()
+    return guess
+
+def right_guess_marker(guess, right_letters, secret_word):
+    index = 0
+    for letter in secret_word:
+        if (guess == letter):
+            right_letters[index] = letter
+        index += 1
+
+def print_victory_message():
+    print("Você ganhou!")
+
+def print_defeat_message():
+    print("Você perdeu!")
+    print("A palavra secreta era: ", secret_word)
 
 if (__name__ == "__main__"):
     jogar()
