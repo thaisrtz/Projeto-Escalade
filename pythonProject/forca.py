7bae52f2776efa1a90c0bf6b269f460b7c5979be
thaisrtz <thaistzanella@gmail.com>
2023-03-01 14:30:41 -0300

    01.03

--- pythonProject/forca.py
+++ pythonProject/forca.py
@@ -1,29 +1,45 @@
 def jogar():
     print("*****************************")
-    print ("Bem vindo ao jogo de forca")
+    print("Bem vindo ao jogo de forca")
     print("*****************************")
 
-    secret_word = "banana"
-    right_letters = ["_", "_", "_", "_", "_", "_"]
+    secret_word = "maca".upper()
+    right_letters = []
 
     defeat = False
     victory = False
+    wrong_guess = 0
+
+    for letter in secret_word:
+        right_letters.append("_")
 
     print(right_letters)
-    while(not defeat and not victory):
+    while (not defeat and not victory):
 
-        trial = input("Digite uma letra: ")
-        trial = trial.strip( )
+        guess = input("Digite uma letra: ")
+        guess = guess.strip().upper()
 
-        index = 0
-        for letter in secret_word:
-            if(trial.upper()==letter.upper()):
-               right_letters[index] = letter
-            index = index + 1
+        if(guess in secret_word):
+            index = 0
+            for letter in secret_word:
+                if (guess == letter):
+                    right_letters[index] = letter
+                index += 1
 
+        else:
+            wrong_guess += 1
+            print ("Você errou! Restam {} tentativas.".format(6-wrong_guess))
+
+        defeat = wrong_guess == 6
+        victory = "_" not in right_letters
         print(right_letters)
 
     print("Fim do jogo!")
+    if (victory):
+          print("Você ganhou!")
+    if (defeat):
+          print ("Você perdeu!")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
