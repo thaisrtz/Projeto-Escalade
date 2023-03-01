08798116c143556e71836768c35e3631bbd3deff
thaisrtz <thaistzanella@gmail.com>
2023-03-01 17:39:37 -0300

    01.03

--- pythonProject/forca.py
+++ pythonProject/forca.py
@@ -1,21 +1,11 @@
 import random
+
+
 def jogar():
-    print("*****************************")
-    print("Bem vindo ao jogo de forca")
-    print("*****************************")
 
-    archive = open ("words.txt", "r")
-    words = []
-    for line in archive:
-        line = line.strip()
-        words.append(line)
-
-    archive.close()
-
-    num = random.randrange (0,len(words))
-    secret_word = words[num].upper()
-
-    right_letters = ["_" for letter in secret_word]
+    print_opening_message()
+    secret_word = secret_word_generator()
+    right_letters = secret_right_letters(secret_word)
 
     defeat = False
     victory = False
@@ -44,10 +34,31 @@ def jogar():
 
     print("Fim do jogo!")
     if (victory):
-          print("Você ganhou!")
+        print("Você ganhou!")
     if (defeat):
-          print ("Você perdeu!")
+        print ("Você perdeu!")
+        print("A palavra secreta era: ", secret_word)
 
+def print_opening_message():
+    print("*****************************")
+    print("Bem vindo ao jogo de forca")
+    print("*****************************")
+
+def secret_word_generator():
+    archive = open("words.txt", "r")
+    words = []
+    for line in archive:
+        line = line.strip()
+        words.append(line)
+
+    archive.close()
+
+    num = random.randrange(0, len(words))
+    secret_word = words[num].upper()
+    return secret_word
+
+def secret_right_letters(word):
+    return ["_" for letter in word]
 
 if (__name__ == "__main__"):
     jogar()
