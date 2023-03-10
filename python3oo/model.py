f5991499f8dfce7695ef30069cd4dc100e2704c5
thaisrtz <thaistzanella@gmail.com>
2023-03-10 16:17:44 -0300

    10.03

--- python3oo/model.py
+++ python3oo/model.py
@@ -1,55 +1,74 @@
-class Movie:
+class Program:
+    def __init__(self, name, year):
+        self._name = name.title()
+        self.year = year
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def liked(self):
+        self._likes += 1
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name.title()
+
+    def __str__(self):
+        return f'{self._name} - Year: {self.year} - {self._likes} likes'
+
+
+class Movie(Program):
     def __init__(self, name, year, duration):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.duration = duration
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def liked(self):
-        self.__likes += 1
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.title()
+    def __str__(self):
+        return f'{self._name} - Year: {self.year} - Duration: {self.duration} min - {self._likes} likes'
 
 
-class Show:
+class Show(Program):
     def __init__(self, name, year, seasons):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.seasons = seasons
-        self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
+    def __str__(self):
+        return f'{self._name} - Year: {self.year} - Seasons: {self.seasons} - {self._likes} likes'
 
-    def liked(self):
-        self.__likes += 1
 
-    @property
-    def name(self):
-        return self.__name
+class Playlist:
+    def __init__(self, name, programs):
+        self.name = name
+        self.programs = programs
 
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.title()
+    def size(self):
+        return len(self.programs)
 
 
 psycho = Movie('psycho', 1960, 109)
-psycho.liked()
-print(f'Name: {psycho.name} - Year: {psycho.year} - Duration: {psycho.duration} minutes - Likes: {psycho.likes}')
-
-
+vertigo = Movie('vertigo', 1958, 128)
+fightclub =Movie('fight club', 1999, 139)
 sopranos = Show('the sopranos', 1999, 6)
+sharpobjects = Show('sharp objects', 2018, 1)
+hunterxhunter = Show('hunter x hunter', 2011, 5)
+
+psycho.liked()
+vertigo.liked()
+fightclub.liked()
+fightclub.liked()
 sopranos.liked()
-sopranos.liked()
-print(f'Name: {sopranos.name} - Year: {sopranos.year} - Seasons: {sopranos.seasons} - Likes: {sopranos.likes}')
+sharpobjects.liked()
+sharpobjects.liked()
+hunterxhunter.liked()
+hunterxhunter.liked()
+
+movies_and_shows = [psycho, vertigo, fightclub, sopranos, sharpobjects, hunterxhunter]
+program_playlist = Playlist('programs playlist', program_playlist)
+
+for program in program_playlist.programs:
+    print(program)
